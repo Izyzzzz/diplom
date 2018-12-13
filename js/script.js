6eb55b1e80bcc16388767df0ae668d5d3528e93f
Izyzzzz <izyzzzz@gmail.com>
2018-12-13 10:59:44 +0200

    second card

--- js/script.js
+++ js/script.js
@@ -5,13 +5,22 @@ window.addEventListener('DOMContentLoaded', function () {
     // Big slider
     let slideIndex = 1,
         slides = document.querySelectorAll('.page > div'),
-        logo = document.querySelectorAll('.sidecontrol a');
+        modules = document.querySelectorAll('.module'),
+        logo = document.querySelectorAll('.sidecontrol a'),
+        next = document.querySelectorAll('.nextmodule'),
+        prev = document.querySelectorAll('.prevmodule');
 
     showSlides(slideIndex);
 
     function showSlides(n) {
-        slides.forEach((item) => item.style.display = 'none');
-        slides[slideIndex - 1].style.display = 'block';
+        if (!slides.length) {
+            modules.forEach((item) => item.style.display = 'none');
+            modules[slideIndex - 1].style.display = 'block';
+        } else {
+            slides.forEach((item) => item.style.display = 'none');
+            slides[slideIndex - 1].style.display = 'block';
+
+        }
     }
 
     function plusSlides(n) {
@@ -21,15 +30,22 @@ window.addEventListener('DOMContentLoaded', function () {
     for (let i = 1; i <= logo.length; i = i + 2) {
 
         logo[i].addEventListener('click', function () {
-            if (i + 1 == logo.length) {
-                showSlides(slideIndex = 1);
-            } else {
-                plusSlides(1);
-            }
+            (i + 1 == logo.length) ? showSlides(slideIndex = 1) : plusSlides(1)
         });
 
         logo[i - 1].addEventListener('click', function () {
             showSlides(slideIndex = 1);
+        });
+    }
+
+    for (let i = 1; i <= next.length; i++) {
+
+        next[i - 1].addEventListener('click', function () {
+            (i == next.length) ? showSlides(slideIndex = 1): plusSlides(1)
+        });
+
+        prev[i - 1].addEventListener('click', function () {
+            (i == 1) ? showSlides(slideIndex = next.length): plusSlides(-1)
         });
     }
 
