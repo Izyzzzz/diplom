2faf96e4d38535559ed8fcc26417a8c30fe7f6e2
Izyzzzz <izyzzzz@gmail.com>
2018-12-13 14:02:54 +0200

    third card

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ window.addEventListener('DOMContentLoaded', function () {
     for (let i = 1; i <= logo.length; i = i + 2) {
 
         logo[i].addEventListener('click', function () {
-            (i + 1 == logo.length) ? showSlides(slideIndex = 1) : plusSlides(1)
+            (i + 1 == logo.length) ? showSlides(slideIndex = 1) : plusSlides( 1 );
         });
 
         logo[i - 1].addEventListener('click', function () {
@@ -41,12 +41,31 @@ window.addEventListener('DOMContentLoaded', function () {
     for (let i = 1; i <= next.length; i++) {
 
         next[i - 1].addEventListener('click', function () {
-            (i == next.length) ? showSlides(slideIndex = 1): plusSlides(1)
+            (i == next.length) ? showSlides(slideIndex = 1): plusSlides( 1 );
         });
 
         prev[i - 1].addEventListener('click', function () {
-            (i == 1) ? showSlides(slideIndex = next.length): plusSlides(-1)
+            (i == 1) ? showSlides(slideIndex = next.length): plusSlides( -1 );
         });
     }
+
+
+    //Video demonstration
+
+    let play = document.querySelectorAll('.play'),
+        overlay = document.querySelector('.overlay'),
+        frameVideo = document.getElementById('frame'),
+        closeBtn = document.querySelector('.close');
+
+    for( let i = 0; i < play.length; i++) {
+        play[i].addEventListener('click', function(){
+            overlay.style.display = 'block';
+            frameVideo.src = play[i].dataset.url;
+        });        
+    }
+
+    closeBtn.addEventListener('click', function() {
+        overlay.style.display = 'none';
+    });    
 
 });
