ff8e1cd1726e9ad5e98f13eaef2dee40a097a6ee
Izyzzzz <izyzzzz@gmail.com>
2018-12-15 16:08:09 +0200

    fifteen card

--- js/script.js
+++ js/script.js
@@ -400,16 +400,19 @@ window.addEventListener('DOMContentLoaded', function () {
     //-----------------------------------------------------------------------------------------------
     // File download
 
-    // let fileDownload = document.querySelectorAll('.download');
+    let fileDownload = document.querySelectorAll('.download');
     
-    // if (!slides.length) {
-    // for(let i = 0; i < fileDownload.length; i++) {
-    //     let newModuled = document.createElement('a');
-    //     fileDownload[i].insertBefore(newModuled, fileDownload[i].children[0]);
-    //     newModuled.setAttribute('download', 'download');
-    //     newModuled.setAttribute('href','/Hello.pdf');
-    //     // newModuled.onclick();
-    // }}
+    if (!slides.length) {
+    for(let i = 0; i < fileDownload.length; i++) {
+        // let newModuled = document.createElement('a');
+        // fileDownload[i].insertBefore(newModuled, fileDownload[i].children[0]);
+        fileDownload[i].setAttribute('download', 'download');
+        // newModuled.setAttribute('href','/Hello.pdf');
+        // newModuled.onclick();
+        fileDownload[i].addEventListener('click', function() {
+            document.location = '/Hello.zip';
+        });
+    }}
 
 
 });
